2464f83c404b875199dd878732634aa59c453738
user <user@example.com>
2025-10-26 03:58:25 +0300

    1

--- semaphore/init.sh
+++ semaphore/init.sh
@@ -3,5 +3,5 @@ echo "SEMAPHORE_ADMIN_PASSWORD=$PASSWORD" >> .env
 TOKEN=$(openssl rand -hex 32)
 echo "SEMAPHORE_RUNNER_REGISTRATION_TOKEN=$TOKEN" >> .env
 
-mkdir semaphore_data
-touch semaphore_data/config.json
+mkdir semaphore_config
+touch semaphore_config/config.json
