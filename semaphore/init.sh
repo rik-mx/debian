55375f8604585057af79ac91f26a2a4456d283c7
user <user@example.com>
2025-10-26 04:08:02 +0300

    1

--- semaphore/init.sh
+++ semaphore/init.sh
@@ -4,4 +4,6 @@ TOKEN=$(openssl rand -hex 32)
 echo "SEMAPHORE_RUNNER_REGISTRATION_TOKEN=$TOKEN" >> .env
 
 mkdir semaphore_config
+mkdir semaphore_data
 sudo chown -R 1001:1001 ./semaphore_config
+sudo chown -R 1001:1001 ./semaphore_data
